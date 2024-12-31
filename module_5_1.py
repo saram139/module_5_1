baee7488f0d41e3b2177a13c893e10d4e287183b
Mikhail Kopylov <saram139@mail.ru>
2024-12-31 21:47:48 +0300

    1.0

---
+++ module_5_1.py
@@ -0,0 +1,19 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.new_floor = None
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        self.new_floor = new_floor
+        if self.new_floor > self.number_of_floors or self.new_floor < 1:
+            print("Такого этажа не существует")
+        else:
+            for i in range(self.new_floor):
+                print(i + 1)
+
+
+h1 = House("ЖК Горский", 18)
+h2 = House("Домик в деревне", 2)
+h1.go_to(5)
+h2.go_to(10)
